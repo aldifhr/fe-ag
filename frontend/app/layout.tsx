3e2c6308ed7a96f6bc69d9d17bda88a416c66d55
aldifhr <aldifahriziilmawan@gmail.com>
2026-06-28 19:52:57 +0700

    fix: correct title in metadata for consistency

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "ManhwAGG",
+  title: "Manhwa.agg",
   description: "An aggregator for manga/manhwa/manhua from various sources.",
 };
 
